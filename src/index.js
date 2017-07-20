686a4cf47f5b29eace5e8add9e3bb146de5ba34b
Geoff Hing <geoffhing@gmail.com>
2017-07-20 18:48:25 -0500

    Factor "rendering" into separate functions
    Factor each piece of "rendering" logic, which is actually just toggling
    classes, into a separate function to make the functionality more
    readable/testable.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,56 @@ function getStatusFromEl(el) {
   return el.getAttribute('data-status');
 }
 
+const renderCategories = function categoriesRenderer(el, state) {
+  state.categories.forEach((category) => {
+    let classFn = removeClass;
+
+    if (category.collapsed) {
+      classFn = addClass;
+    }
+
+    const categoryNameEl = el.querySelectorAll(
+      `.category[data-slug="${category.slug}"] .category__name`,
+    );
+    classFn(categoryNameEl, 'category__name--collapsed');
+
+    const recommendationEls = el.querySelectorAll(
+      `.category[data-slug="${category.slug}"] .recommendation`,
+    );
+    classFn(recommendationEls, 'recommendation--collapsed');
+  });
+
+  return el;
+};
+
+const applyStatusFilters = function statusFilterRenderer(el, state) {
+  addClass(
+    el.querySelectorAll('.recommendation'),
+    'recommendation--hidden',
+  );
+
+  removeClass(
+    el.querySelectorAll('.status-filter'),
+    'status-filter--selected',
+  );
+
+  state.statuses.forEach((statusFilter) => {
+    removeClass(
+      el.querySelectorAll(
+        `.recommendation[data-status="${statusFilter}"]`,
+      ),
+      'recommendation--hidden',
+    );
+
+    addClass(
+      el.querySelectorAll(
+        `.status-filter[data-status="${statusFilter}"]`,
+      ),
+      'status-filter--selected',
+    );
+  });
+};
+
 // eslint-disable-next-line import/prefer-default-export
 export class App {
   constructor(container) {
@@ -52,53 +102,11 @@ export class App {
     this.render();
   }
 
-
   render() {
     const state = this.store.getState();
 
-    state.categories.forEach((category) => {
-      let classFn = removeClass;
-
-      if (category.collapsed) {
-        classFn = addClass;
-      }
-
-      const categoryNameEl = this.container.querySelectorAll(
-        `.category[data-slug="${category.slug}"] .category__name`,
-      );
-      classFn(categoryNameEl, 'category__name--collapsed');
-
-      const recommendationEls = this.container.querySelectorAll(
-        `.category[data-slug="${category.slug}"] .recommendation`,
-      );
-      classFn(recommendationEls, 'recommendation--collapsed');
-    });
-
-    addClass(
-      this.container.querySelectorAll('.recommendation'),
-      'recommendation--hidden',
-    );
-
-    removeClass(
-      this.container.querySelectorAll('.status-filter'),
-      'status-filter--selected',
-    );
-
-    state.statuses.forEach((statusFilter) => {
-      removeClass(
-        this.container.querySelectorAll(
-          `.recommendation[data-status="${statusFilter}"]`,
-        ),
-        'recommendation--hidden',
-      );
-
-      addClass(
-        this.container.querySelectorAll(
-          `.status-filter[data-status="${statusFilter}"]`,
-        ),
-        'status-filter--selected',
-      );
-    });
+    renderCategories(this.container, state);
+    applyStatusFilters(this.container, state);
   }
 
   clickCategoryName(evt) {
