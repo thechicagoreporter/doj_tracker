21bc2c991fc6541cfff6e9401c0cf6a03c6b93ff
Geoff Hing <geoffhing@gmail.com>
2017-09-02 22:13:20 -0500

    Suppress search form submission
    Prevent accidental page reloads by supressing the for submit event.
    
    Closes https://app.asana.com/0/0/420363384864037/f

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './SearchForm.css';
 
+/**
+ * Incremental search for recommendations.
+ */
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
@@ -10,11 +13,13 @@ class SearchForm extends React.Component {
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
     return (
-      <form className={styles.form}>
+      <form className={styles.form}
+        onSubmit={this.handleSubmit}>
         <input name="q"
           className={styles.searchInput}
           type="text"
@@ -25,10 +30,24 @@ class SearchForm extends React.Component {
     );
   }
 
+  /**
+   * React lifecycle event after a componet updates.
+   *
+   * In our case, the update happens after we update state in
+   * `handleInputChange()`.
+   *
+   * Call the `onSearch` handler to pass the state change upstream.
+   */
   componentDidUpdate() {
     this.props.onSearch(this.state.q);
   }
 
+  /**
+   * Handler for input change event.
+   *
+   * @param {change} evt - change DOM event.
+   * @listens change
+   */
   handleInputChange(evt) {
     const target = evt.target;
 
@@ -36,9 +55,27 @@ class SearchForm extends React.Component {
       [target.name]: target.value,
     });
   }
+
+  /**
+   * Handler for form submit event.
+   *
+   * This is a noop.  Our search is actually kicked off by change events on the
+   * `q` input. We want to make sure we don't submit te form so the page
+   * reloads.
+   *
+   * @param {submit} evt - submit DOM event.
+   * @listens submit
+   */
+  // eslint-disable-next-line class-methods-use-this
+  handleSubmit(evt) {
+    evt.preventDefault();
+  }
 }
 
 SearchForm.propTypes = {
+  /**
+   * Search callback. Takes the query string as an argument.
+   */
   onSearch: PropTypes.func,
 };
 
