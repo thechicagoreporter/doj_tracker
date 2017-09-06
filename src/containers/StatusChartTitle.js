bf8e0ba87eb38de013c9004b53e215626f1add23
Geoff Hing <geoffhing@gmail.com>
2017-09-06 00:27:25 -0500

    More space between chart title and legend
    Addresses https://app.asana.com/0/0/406067886364911/f

--- src/containers/StatusChartTitle.js
+++ src/containers/StatusChartTitle.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import styles from './StatusChartTitle.css';
 
 const mapStateToProps = state => ({
   text: state.chartTitle,
 });
 
 const StatusChartTitle = ({ text }) => (
-  text ? <h3>{text}</h3> : false
+  text ? <h3 className={styles.title}>{text}</h3> : false
 );
 
 StatusChartTitle.propTypes = {
